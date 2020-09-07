e37c9f5c50adb0d0db42e826c5b7d04bc30e28ac
Thais Minas <thaisminasds@gmail.com>
2020-09-07 18:23:24 -0300

    refatoração de código

--- calculadora.js
+++ calculadora.js
@@ -4,19 +4,31 @@ $(document).ready(function() {
         let botao = e.target.value;
         let resultado = $("#resultado").val();
 
-        $("#resultado").val(resultado + botao);
+        if (botao != "Del" && botao != "ANS") {
+            $("#resultado").val(resultado + botao);
+        }
 
         if (botao == "=") {
             $("#resultado").val(eval(resultado));
         }
+
         if (botao == "C") {
             $("#resultado").val("");
         }
-        if (botao == "Del") {
-            botao.substring(0, -3)
 
+        if (botao == "Del") {
+            let tamanho = resultado.length;
+
+            resultado = resultado.substring(0, tamanho - 1);
+            $("#resultado").val(resultado);
         }
 
-
+        if (botao == "ANS") {
+            if (localStorage.getItem("ANS") == "") {
+                localStorage.setItem("ANS", resultado);
+            } else {
+                $("#resultado").val(localStorage.getItem("ANS"));
+            }
+        }
     });
 });
